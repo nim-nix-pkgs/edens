de28753adfd80083896608e5f85b5abeaec393fc
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:15 -0300

    chore: re index edens

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."edens-master".dir   = "master";
+  inputs."edens-master".owner = "nim-nix-pkgs";
+  inputs."edens-master".ref   = "master";
+  inputs."edens-master".repo  = "edens";
+  inputs."edens-master".type  = "github";
+  inputs."edens-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."edens-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."edens-1_0_0".dir   = "1_0_0";
+  inputs."edens-1_0_0".owner = "nim-nix-pkgs";
+  inputs."edens-1_0_0".ref   = "master";
+  inputs."edens-1_0_0".repo  = "edens";
+  inputs."edens-1_0_0".type  = "github";
+  inputs."edens-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."edens-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
